125df025d85379f85cbafd7e094a16e0513db5e9
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-06-08 00:14:05 +0300

    modified in Calendar Module .py because there are some error in final output

--- HackerRank/Level-Easy/Calendar Module .py
+++ HackerRank/Level-Easy/Calendar Module .py
@@ -1,10 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import calendar
 from  datetime import datetime
-d = input()
-d = datetime.strptime(d, '%d %m %Y')
+date = input()
+date = datetime.strptime(date, '%m %d %Y')
 
-# print(d.day)
-# print(d.strftime('%A'))
-print(calendar.day_name[calendar.weekday(d.year, d.month, d.day)])
+print(calendar.day_name[calendar.weekday(date.year, date.month, date.day)].upper())
 
