c029c806539f224e04ae2a3a91f5c68679e822fb
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-06-21 23:33:46 +0300

    uploaded Convert_a_Boolean_to_a_String.py to convert boolean value to string

---
+++ Codewars/8kyu/Convert_a_Boolean_to_a_String.py
@@ -0,0 +1,15 @@
+"""
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+"""
+
+# First solution 
+
+def boolean_to_string(b):
+    return "True" if b == 1 else "False"
+
+# Second solution 
+def boolean_to_string(b):
+    return "True" if b else "False"
