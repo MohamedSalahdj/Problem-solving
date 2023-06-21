966886d5762f66f515302ec522667a0d91d8170a
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-06-21 23:40:39 +0300

    added other solution without  use any bulit in method or use list

--- Codewars/8kyu/Reversed_Strings.py
+++ Codewars/8kyu/Reversed_Strings.py
@@ -4,7 +4,19 @@
         'word'   =>  'drow'
 """
 # first solution 
+# def solution(string):
+#     l = [letter for letter in string]
+#     l.reverse()
+#     return ''.join(l)
+
+#second Solution 
+
 def solution(string):
-    l = [letter for letter in string]
-    l.reverse()
-    return ''.join(l)
+    i = len(string) - 1
+    new_string = ''
+    for _ in range(len(string)):
+        new_string+= string [i]
+        i-=1
+    return new_string
+
+print(solution('world'))
