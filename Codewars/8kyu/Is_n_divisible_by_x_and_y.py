b5a520bc0c0c22a9852ca7e0ed4572481007592d
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-06-21 23:07:19 +0300

    added other solution without if and else

--- Codewars/8kyu/Is_n_divisible_by_x_and_y.py
+++ Codewars/8kyu/Is_n_divisible_by_x_and_y.py
@@ -8,13 +8,12 @@ Examples:
 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
 """
-
+# First solution 
 def is_divisible(n,x,y):
     return True if n % x == 0 and n % y ==0 else False
 
-print(is_divisible(3,2,2))  # False
-print(is_divisible(3,3,4))  # False
-print(is_divisible(12,3,4)) # True
-print(is_divisible(8,3,4))  # False
 
-print((1900%100))
+
+# Second Solution 
+def is_divisible(n,x,y):
+    return n%x==0 and n%y==0
