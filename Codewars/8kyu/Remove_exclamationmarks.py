72fd90b6e63976d258099ed50adfe50a29d76a24
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-06-22 18:38:53 +0300

    uploaded Remove_exclamationmarks.py file to remove exclamation marks from string

---
+++ Codewars/8kyu/Remove_exclamationmarks.py
@@ -0,0 +1,11 @@
+"""
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+"""
+# first solution 
+def remove_exclamation_marks(s):
+    new_string = ''
+    for letter in s:
+        if letter !='!':
+            new_string+=letter
+    return new_string
+
