a8312acce8e989b155c1a0ebbbbf92ca122b84e5
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-06-23 23:36:37 +0300

    uploaded Beginner-Reduce-but-Grow.py to calculate all item in array

---
+++ Codewars/8kyu/Beginner-Reduce-but-Grow.py
@@ -0,0 +1,13 @@
+"""
+Given a non-empty array of integers,
+return the result of multiplying the values together in order.
+    Example:
+        [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+"""
+#first solution 
+def grow(arr):
+    mul = 1
+    for item in arr:
+        mul*=item 
+    return mul
