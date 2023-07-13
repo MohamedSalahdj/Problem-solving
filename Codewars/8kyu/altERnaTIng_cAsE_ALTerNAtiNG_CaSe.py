6b631524747c52d3898fefa070fd6b747cad882a
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-07-13 14:33:44 +0300

    uploaded altERnaTIng_cAsE_ALTerNAtiNG_CaSe.py to alternative letter case

---
+++ Codewars/8kyu/altERnaTIng_cAsE_ALTerNAtiNG_CaSe.py
@@ -0,0 +1,24 @@
+"""
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+"""
+
+
+def to_alternating_case(string):
+    new_string_after_alternating_case = ''
+    for letter in string:
+        if letter.isupper():
+            new_string_after_alternating_case+=letter.lower()
+        else:
+            new_string_after_alternating_case+=letter.upper()
+    return new_string_after_alternating_case
