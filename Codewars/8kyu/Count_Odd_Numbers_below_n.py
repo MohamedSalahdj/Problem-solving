78f4839e55f2f3418a1b790382eee8d77b114610
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-07-19 19:55:46 +0300

    uploaded count_odd_numbers_below_n.py  with second solution without use built-in function

--- Codewars/8kyu/Count_Odd_Numbers_below_n.py
+++ Codewars/8kyu/Count_Odd_Numbers_below_n.py
@@ -6,6 +6,11 @@ Examples (Input -> Output)
 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
 
 """
-
+# first Slolution 
 def odd_count(n):
    return int(n/2)
+
+
+# Second Slolution 
+def odd_count(n):
+   return (n-1) / 2 if n%2 !=0 else n/2
