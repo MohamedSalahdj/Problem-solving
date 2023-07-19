543bfa71182a8504ccf47bdbad8b5703beaa55a8
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-07-19 19:38:47 +0300

    uploaded Square(n)_Sum.py with First solution :

---
+++ Codewars/8kyu/Square(n)_Sum.py
@@ -0,0 +1,14 @@
+"""
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9
+
+"""
+#First Solution 
+def square_sum(numbers):
+    sum_of_number = 0
+    for number in numbers:
+        sum_of_number+= number*number
+    return sum_of_number
+
+
