54c755a16049b1be7543ee4c8d22cd4cfb854694
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-07-22 23:20:11 +0300

    uploaded what-is-between.py

---
+++ Codewars/8kyu/What-is-between.py
@@ -0,0 +1,15 @@
+"""
+Complete the function that takes two integers (a, b, where a < b) 
+and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+
+"""
+
+def between(a,b):
+    l = [ number for number in range(a,b+1)]
+    return l 
