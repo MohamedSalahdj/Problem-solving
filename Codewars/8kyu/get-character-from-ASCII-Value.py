17d5af1232578911868c0950050e47f5b49948ec
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-07-22 23:27:58 +0300

    uploaded get-character-from-ASCII-Value.py

---
+++ Codewars/8kyu/get-character-from-ASCII-Value.py
@@ -0,0 +1,14 @@
+"""
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+For ASCII table, you can refer to http://www.asciitable.com/
+
+"""
+
+def get_char(c):
+    return chr(c)
