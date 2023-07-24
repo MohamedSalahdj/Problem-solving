f929f4e081c725d065a2505c648d5701cf6ced64
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-07-24 16:32:24 +0300

    uploaded MakeUpperCase.py First Solution

---
+++ Codewars/8kyu/MakeUpperCase.py
@@ -0,0 +1,47 @@
+"""
+Write a function which converts the input string to uppercase.
+"""
+# First solution 
+def make_upper_case(s):
+    return s.upper()
+
+# SecondSolution 
+def make_upper_case(s):
+    string_with_upper_case = ''
+    letters = {
+        "a" : "A",
+        "b" : "B",
+        "c" : "C",
+        "d" : "D",
+        "e" : "E",
+        "f" : "F",
+        "g" : "G",
+        "h" : "H",
+        "i" : "I",
+        "j" : "J",
+        "h" : "H",
+        "i" : "I",
+        "j" : "J",
+        "k" : "K",
+        "l" : "L",
+        "m" : "M",
+        "n" : "N",
+        "o" : "O",
+        "p" : "P",
+        "q" : "Q",
+        "r" : "R",
+        "s" : "S",
+        "t" : "T",
+        "u" : "U",
+        "v" : "V",
+        "w" : "W",
+        "x" : "X",
+        "y" : "Y",
+        "z" : "Z",
+    }
+    for letter in s:
+        if letter in letters:
+            string_with_upper_case+= letters[letter]
+        else:
+            string_with_upper_case+=letter
+    return string_with_upper_case
