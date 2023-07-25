191bf88563077da1444dcd8589b812278072091e
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-07-25 03:06:54 +0300

    uploaded Switch-it-Up.py with First solition

---
+++ Codewars/8kyu/Switch-it-Up.py
@@ -0,0 +1,26 @@
+"""
+When provided with a number between 0-9,
+return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+"""
+
+# First Solution
+def switch_it_up(number):
+    word_number = {
+        0 : 'Zero',
+        1: 'One',
+        2 : 'Two',
+        3: 'Three',
+        4: 'Four',
+        5 : 'Five',
+        6 : 'Six',
+        7 : 'Seven',
+        8 : 'Eight',
+        9 : 'Nine'
+    }
+    return word_number[number]
