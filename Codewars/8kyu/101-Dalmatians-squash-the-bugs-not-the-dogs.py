1ee790c5dadb837cd93e662801b1cbba3f1f4922
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-02 19:13:17 +0300

    uploaded 101-Dalmatians-squash-the-bugs-not-the-dogs.py with second-solution

--- Codewars/8kyu/101-Dalmatians-squash-the-bugs-not-the-dogs.py
+++ Codewars/8kyu/101-Dalmatians-squash-the-bugs-not-the-dogs.py
@@ -14,4 +14,13 @@ def how_many_dalmatians(n):
     respond = dogs[0] if n <= 10 else dogs[1] if n <= 50 else dogs[3] if n == 101  else dogs[2]
     return respond
 
-#second-solution 
+#second-solution to explain 
+def how_many_dalmatians(n):
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+
+    if n <= 10 : return dogs[0]
+    elif n <= 50 : return dogs[1]
+    elif n == 101 : return dogs[3]
+    else :  
+        return dogs[2]
+        
