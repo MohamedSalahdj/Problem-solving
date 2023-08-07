202a1cbc0a88f340093b16246aa08fe88352cd94
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-07 16:41:12 +0300

    uploaded Reversed-sequence.py

---
+++ Codewars/8kyu/Reversed-sequence.py
@@ -0,0 +1,7 @@
+# Build a function that returns an array of integers from n to 1 where n>0.
+
+# Example : n=5 --> [5,4,3,2,1]
+
+def reverse_seq(n):
+    l = [ (n-i) for i in range(n)]
+    return l
