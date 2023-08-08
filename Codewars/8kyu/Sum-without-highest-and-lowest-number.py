a08e6be3219fb515d7cccb4f68409c072637f018
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-08 22:13:50 +0300

    uploaded Sum-without-highest-and-lowest-number.py with first-solution

---
+++ Codewars/8kyu/Sum-without-highest-and-lowest-number.py
@@ -0,0 +1,23 @@
+"""
+Sum all the numbers of a given array ( cq. list ), 
+except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, 
+even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+
+"""
+#first-soluton 
+def sum_array(arr):
+    if arr not in ([],None) and len(arr) != 1 :
+        arr.remove(min(arr))
+        arr.remove(max(arr))
+        return sum(arr)
+    else:
+        return 0
+    
