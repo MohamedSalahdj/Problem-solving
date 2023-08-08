b12fd875e9bfb60e4f5593d6d3b621c4f586fb73
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-08 22:26:24 +0300

    uploaded Find-the-smallest-integer-in-the-array.py with first-solution

---
+++ Codewars/8kyu/Find-the-smallest-integer-in-the-array.py
@@ -0,0 +1,18 @@
+"""
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+"""
+
+#first-solution 
+def find_smallest_int(arr):
+    min = arr[0]
+    for integer in arr:
+        if integer < min :
+            min = integer
+    return min
