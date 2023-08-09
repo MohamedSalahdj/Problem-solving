ebb4b913dc4239cccfd6ce4068ffee5442440253
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-09 18:28:40 +0300

    uploaded Fake-Binary.py with first-solution

---
+++ Codewars/8kyu/Fake-Binary.py
@@ -0,0 +1,16 @@
+"""
+Given a string of digits, 
+you should replace any digit below 5 with '0' and any digit 5 and above with '1'. 
+Return the resulting string.
+
+Note: input will never be an empty string
+"""
+
+# first-solution
+def fake_bin(x):
+    string_without_any_number_graeter_or_lower_than5 = ''
+    for i in x :
+        if int(i) < 5 :string_without_any_number_graeter_or_lower_than5+='0'
+        else:string_without_any_number_graeter_or_lower_than5+='1'
+    return string_without_any_number_graeter_or_lower_than5
+            
