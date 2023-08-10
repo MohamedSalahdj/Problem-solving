8088113d5d810efb07fe708a988e335107707868
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-10 20:59:27 +0300

    uploaded Convert-a-string-to-an-array.py with first-solution

---
+++ Codewars/8kyu/Convert-a-string-to-an-array.py
@@ -0,0 +1,14 @@
+"""
+Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+"""
+
+# first-solution with built-in-function
+def string_to_array(s):
+    return s.split() if len(s) > 0  else [""]
+
+
