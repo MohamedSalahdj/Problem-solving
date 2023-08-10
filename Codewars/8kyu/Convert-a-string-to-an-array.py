4ef694d90ffd024a7643cd3717684fd9d01740f5
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-10 21:10:00 +0300

    uploaded Convert-a-string-to-an-array.py with second-solution

--- Codewars/8kyu/Convert-a-string-to-an-array.py
+++ Codewars/8kyu/Convert-a-string-to-an-array.py
@@ -8,7 +8,21 @@ Examples (Input ==> Output):
 """
 
 # first-solution with built-in-function
-def string_to_array(s):
-    return s.split(" ") 
+# def string_to_array(s):
+#     return s.split(" ") 
 
+#Second-solution without split
+def string_to_array(s):
+    l = []
+    i = 0
+    word = ''
+    while i < len(s):
+        if s[i] != ' ':
+            word+=s[i]
+        else:
+            l.append(word)
+            word = ''
+        i+=1
+    l.append(word)
+    return l
 
