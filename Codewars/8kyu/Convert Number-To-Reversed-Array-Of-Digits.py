fb7bb79506e895834579b1da406a1b6cc1bd9cc3
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-14 06:31:46 +0300

    Convert Number-To-Reversed-Array-Of-Digits.py with First-solution

---
+++ Codewars/8kyu/Convert Number-To-Reversed-Array-Of-Digits.py
@@ -0,0 +1,14 @@
+# Convert number to reversed array of digits
+# Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+# Example(Input => Output):
+# 35231 => [1,3,2,5,3]
+# 0 => [0]
+
+
+
+#First-solution 
+def digitize(n):
+    l = [int(num) for num in str(n)]
+    l.reverse()
+    return l
