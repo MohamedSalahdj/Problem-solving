a32b33c874c56317633d9b429de4ce4595b281d2
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-14 06:42:53 +0300

    added Reversed-Words.py with first-solution

---
+++ Codewars/8kyu/Reversed-Words.py
@@ -0,0 +1,16 @@
+"""
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+"""
+
+
+#first-solution 
+def reverse_words(s):
+    l_of_word = list(reversed(s.split(' ')))
+    return ' '.join(l_of_word)
