b255757a66cfff74d65437ce1f55319e1216feb2
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-15 22:30:57 +0300

    uploaded Is-it-a-palindrome.py with second solution

--- Codewars/8kyu/Is-it-a-palindrome.py
+++ Codewars/8kyu/Is-it-a-palindrome.py
@@ -7,3 +7,48 @@ such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A
 #first-solution
 def is_palindrome(s):
     return s.upper() == s.upper()[::-1] 
+
+
+def is_palindrome(s):
+    letters = {
+        "a" : "A",
+        "b" : "B",
+        "c" : "C",
+        "d" : "D",
+        "e" : "E",
+        "f" : "F",
+        "g" : "G",
+        "h" : "H",
+        "i" : "I",
+        "j" : "J",
+        "h" : "H",
+        "i" : "I",
+        "j" : "J",
+        "k" : "K",
+        "l" : "L",
+        "m" : "M",
+        "n" : "N",
+        "o" : "O",
+        "p" : "P",
+        "q" : "Q",
+        "r" : "R",
+        "s" : "S",
+        "t" : "T",
+        "u" : "U",
+        "v" : "V",
+        "w" : "W",
+        "x" : "X",
+        "y" : "Y",
+        "z" : "Z",
+    }
+    # convert input string to upper case 
+    string_left_to_right_upper = ''
+    for letter in s:
+        if letter in letters:
+            string_left_to_right_upper += letters[letter]
+        else:
+            string_left_to_right_upper+=letter
+    # reverse string 
+    string_right_to_left_upper = string_left_to_right_upper[::-1]
+
+    return string_left_to_right_upper == string_right_to_left_upper
