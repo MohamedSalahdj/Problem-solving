89569580a815359ba01d526507bf0c917c7f77df
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-15 21:06:21 +0300

    uploaded mutations with first-solution

---
+++ HackerRank/Level-Easy/Mutations.py
@@ -0,0 +1,43 @@
+"""
+
+Read a given string, change the character at a given index and then print the modified string.
+Function Description
+
+Complete the mutate_string function in the editor below.
+
+mutate_string has the following parameters:
+
+string string: the string to change
+int position: the index to insert the character at
+string character: the character to insert
+Returns
+
+string: the altered string
+Input Format
+
+The first line contains a string, .
+The next line contains an integer , the index location and a string , separated by a space.
+
+Sample Input
+
+STDIN           Function
+-----           --------
+abracadabra     s = 'abracadabra'
+5 k             position = 5, character = 'k'
+Sample Output
+
+abrackdabra
+
+"""
+
+# First-solution 
+def mutate_string(string, position, character):
+    l = list(string)
+    l[position] = character
+    return "".join(l)
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
