e0821fd8bd804f441e1e5195c35806c328b6098f
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-15 22:25:03 +0300

    uploaded Is-it-a-palindrome.py

--- Codewars/8kyu/Is-it-a-palindrome.py
+++ Codewars/8kyu/Is-it-a-palindrome.py
@@ -4,11 +4,6 @@ A palindrome is a word, number, phrase, or other sequence of symbols that reads 
 such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".
 
 """
-
-dic_of_letter = {
-    'A':'a',
-    'B':'b'
-}
-name = "A"
-name2 = 'a'
-print(dic_of_letter[name] == name2)
+#first-solution
+def is_palindrome(s):
+    return s.upper() == s.upper()[::-1] 
