1248ba8be45a239441f472e81e5d29742a632e17
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-15 22:23:50 +0300

    uploaded Is-it-a-palindrome.py

---
+++ Codewars/8kyu/Is-it-a-palindrome.py
@@ -0,0 +1,14 @@
+"""
+Write a function that checks if a given string (case insensitive) is a palindrome. 
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, 
+such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".
+
+"""
+
+dic_of_letter = {
+    'A':'a',
+    'B':'b'
+}
+name = "A"
+name2 = 'a'
+print(dic_of_letter[name] == name2)
