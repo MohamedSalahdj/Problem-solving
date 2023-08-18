f7020ac6d2d56b6e2b0091fb01b97653eac8eaec
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-19 02:17:20 +0300

    Student's-Final-Grade.py modified in code because add some logical operator worng

--- Codewars/8kyu/Student's-Final-Grade.py
+++ Codewars/8kyu/Student's-Final-Grade.py
@@ -25,6 +25,5 @@ Examples(Inputs-->Output):
 *Use Comparison and Logical Operators.
 
 """
-
 def final_grade(exam, projects):
-    return  100 if exam > 90 and projects > 10 else 90 if exam > 75 and projects >= 5 else 75 if exam > 50 and projects >= 2 else 0
+    return  100 if exam > 90 or projects > 10 else 90 if exam > 75 and projects >= 5 else 75 if exam > 50 and projects >= 2 else 0
