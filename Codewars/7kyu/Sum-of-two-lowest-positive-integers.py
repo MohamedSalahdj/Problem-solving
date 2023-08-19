d7e375606b718076fef835e027f42a63b9312d5c
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-20 02:15:59 +0300

    added Sum-of-two-lowest-positive-integers.py second-solution

--- Codewars/7kyu/Sum-of-two-lowest-positive-integers.py
+++ Codewars/7kyu/Sum-of-two-lowest-positive-integers.py
@@ -14,6 +14,7 @@ def get_minimum_number(numbers):
     for item in numbers:
         if item < minimum_number:
             minimum_number = item
+    
     return minimum_number
 
 def sum_two_smallest_numbers(numbers):
@@ -21,7 +22,22 @@ def sum_two_smallest_numbers(numbers):
     two_smallest_numbers.append(get_minimum_number(numbers))
     numbers.remove(get_minimum_number(numbers))
     two_smallest_numbers.append(get_minimum_number(numbers))
+    
     return two_smallest_numbers[0] + two_smallest_numbers[-1]
     
-    
-    
+#second-solution 
+def sum_two_smallest_numbers(numbers):
+    if numbers[0] < numbers[1]:
+        minimum_number_one = numbers[0]
+        minimum_number_two = numbers[1]
+    else:
+        minimum_number_one = numbers[1]
+        minimum_number_two = numbers[0]
+    for i in range(2,len(numbers)):
+        if numbers[i] < minimum_number_one:
+            minimum_number_two = minimum_number_one
+            minimum_number_one = numbers[i]
+        elif numbers[i] < minimum_number_two:
+            minimum_number_two = numbers[i]
+    return minimum_number_one + minimum_number_two
+        
