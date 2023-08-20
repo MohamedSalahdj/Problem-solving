2dc3b2a33beb6bf03cd9ab048ec353cd4562ecff
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-21 01:31:51 +0300

    uploaded Eliminate-the-intruders-Bit-manipulation.py

---
+++ Codewars/7kyu/Eliminate-the-intruders-Bit-manipulation.py
@@ -0,0 +1,24 @@
+"""
+Task
+You are given a string representing a number in binary. 
+Your task is to delete all the unset bits in this string and return the corresponding number (after keeping only the '1's).
+
+In practice, you should implement this function:
+
+def eliminate_unset_bits(number):
+Examples
+eliminate_unset_bits("11010101010101") ->  255 (= 11111111)
+eliminate_unset_bits("111") ->  7
+eliminate_unset_bits("1000000") -> 1
+eliminate_unset_bits("000") -> 0
+
+"""
+def eliminate_unset_bits(number):
+    l = [ i for i in number if i == '1']
+    result = 0
+    i = 0 
+    while i <len(l):
+        if l[i] == '1':
+            result+= 2**i
+        i+=1
+    return result
