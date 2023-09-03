a4f53550f8f07e506fcce61a83fd927e3b90105e
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-03 18:14:12 +0300

    added Printing-Array-elements-with-Comma-delimiters.py

---
+++ Codewars/8kyu/Printing-Array-elements-with-Comma-delimiters.py
@@ -0,0 +1,20 @@
+"""
+DESCRIPTION:
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level
+"""
+
+#first-solution 
+def print_array(arr):
+    sequance = ''
+    for i in range(len(arr)-1):
+        sequance+=str(arr[i]) +','
+    sequance+=str(arr[-1])
+    return sequance
