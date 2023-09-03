546807c70ddd981fb86e082f8204b4cd7d5b19d1
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-04 02:26:26 +0300

    added Removing-Elements.py second-solution

--- Codewars/8kyu/Removing-Elements.py
+++ Codewars/8kyu/Removing-Elements.py
@@ -13,3 +13,8 @@ None of the arrays will be empty, so you don't have to worry about that!
 def remove_every_other(my_list):
     return my_list[::2]
             
+
+#second-solution
+def remove_every_other(my_list):
+    l = [ my_list[i] for i in range(0,len(my_list),2)]
+    return l
