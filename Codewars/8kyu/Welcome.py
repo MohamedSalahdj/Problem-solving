9004e567d98364cd91553e09b8697ddf02ab075a
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-09 08:39:22 +0300

    added welcome.py second-solution

--- Codewars/8kyu/Welcome.py
+++ Codewars/8kyu/Welcome.py
@@ -33,7 +33,7 @@ IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
 IP_ADDRESS_NOT_FOUND - ip address not in the database
 IP_ADDRESS_REQUIRED - no ip address was supplied
 """
-
+#first-solution
 def greet(language):
     greeting = {
     "english": "Welcome",
@@ -56,3 +56,27 @@ def greet(language):
     }
     
     return greeting[language] if language in greeting else "Welcome"
+
+#second-solution
+def greet(language):
+    greeting = {
+    "english": "Welcome",
+    "czech": "Vitejte",
+    "danish": "Velkomst",
+    "dutch": "Welkom",
+    "estonian": "Tere tulemast",
+    "finnish": "Tervetuloa",
+    "flemish": "Welgekomen",
+    "french": "Bienvenue",
+    "german": "Willkommen",
+    "irish": "Failte",
+    "italian": "Benvenuto",
+    "latvian": "Gaidits",
+    "lithuanian": "Laukiamas",
+    "polish": "Witamy",
+    "spanish": "Bienvenido",
+    "swedish": "Valkommen",
+    "welsh": "Croeso",
+    }
+    
+    return greeting.get(language, 'Welcome')
