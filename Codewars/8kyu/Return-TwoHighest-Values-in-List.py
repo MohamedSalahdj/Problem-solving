bade92be29f277609749d56c7b4141384301759a
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-20 01:22:42 +0300

    added Return-TwoHighest-Values-in-List.py second-solution

--- Codewars/8kyu/Return-TwoHighest-Values-in-List.py
+++ Codewars/8kyu/Return-TwoHighest-Values-in-List.py
@@ -9,7 +9,34 @@ Examples:
 []  =>  []
 
 """
+#first-solution
 def two_highest(arg1):
     s = list(set(arg1))
     s.sort(reverse=True)
     return s[:2]
+
+
+#second-solution
+def remove_dublicate(l):
+    for i in range(len(l)):
+        for j in range(i+1,len(l)):
+            if l[i] == l[j]:
+                return remove_dublicate(l[i+1:])
+    return l
+
+def inverse_sort_list(l):
+    for i in range(len(l)):
+        for j in range(i+1,len(l)):
+            if l[i] < l[j]:
+                temp = l[j]
+                l[j] = l[i]
+                l[i] = temp
+    return l
+def two_highest(arg1):
+    arg1 = remove_dublicate(arg1)
+    print(arg1)
+    arg1 = inverse_sort_list(arg1)
+    return arg1[:2]
+
+
+print(two_highest([15, 20, 20, 17]))
