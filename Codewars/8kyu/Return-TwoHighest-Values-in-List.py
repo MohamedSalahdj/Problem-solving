82f42b92af1c023b33fb56ae6255dda070697a46
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-20 00:54:39 +0300

    added Return-TwoHighest-Values-in-List.py

---
+++ Codewars/8kyu/Return-TwoHighest-Values-in-List.py
@@ -0,0 +1,15 @@
+"""
+In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  []
+
+"""
+def two_highest(arg1):
+    s = list(set(arg1))
+    s.sort(reverse=True)
+    return s[:2]
