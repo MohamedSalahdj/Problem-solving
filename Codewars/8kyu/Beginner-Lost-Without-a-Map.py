089b0e39f817224b46734ad872bb1cfb09d3a31b
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-04-17 05:38:25 +0200

    added Beginner-Lost-Without-a-Map.py

---
+++ Codewars/8kyu/Beginner-Lost-Without-a-Map.py
@@ -0,0 +1,12 @@
+"""
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+"""
+
+def maps(a):
+    new_double_array = [num+num for num in a]
+    return new_double_array
+    
