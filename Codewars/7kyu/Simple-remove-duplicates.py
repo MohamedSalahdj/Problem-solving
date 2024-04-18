16a8a0e95013e8e9a4dcd3de23031d1fadfc3a14
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-04-18 04:06:35 +0200

    added firs_solution for Simple-remove-duplicates.py problem

---
+++ Codewars/7kyu/Simple-remove-duplicates.py
@@ -0,0 +1,26 @@
+"""
+Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+Expected output: [4, 6, 3]
+
+More examples can be found in the test cases.
+
+Good luck!
+
+"""
+
+#first_solution
+def solve(arr): 
+    i = 0
+    while i < len(arr):
+        if arr[i] in arr[i+1:]:
+            arr.remove(arr[i])
+        else:
+            i+=1
+    return arr
