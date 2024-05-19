965be54af03eb3c3854b3ed1f78848a3b4247543
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-05-19 15:24:04 +0300

    added Working-with-arrays-I-(and why your code fails in some katas) problem

---
+++ Codewars/7kyu/Working-with-arrays-I-(and why your code fails in some katas).py
@@ -0,0 +1,14 @@
+"""
+DESCRIPTION:
+In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+Good luck!
+
+Hint: watch out for side effects.
+
+Some good reading: Python Lists
+"""
+
+
+def without_last(lst):
+    return lst[:-1]
