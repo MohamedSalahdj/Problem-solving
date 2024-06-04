011fa21492a0baa6ac7e7dc7d126b56df2154e28
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-06-04 12:05:06 +0300

    added Thinking & Testing: A and B? probelm

---
+++ Codewars/7kyu/Thinking&Testing-A-and-B.py
@@ -0,0 +1,21 @@
+''' 
+            problem name : Thinking & Testing: A and B?
+DESCRIPTION:
+    No Story
+
+    No Description
+
+    Only by Thinking and Testing
+
+    Look at result of testcase, guess the code!
+
+'''
+
+
+
+
+
+
+
+def testit (a, b):
+    return a | b 
