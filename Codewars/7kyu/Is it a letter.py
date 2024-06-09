6fd891c5346563548f1efbdba5bf25c0cb8f4274
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-06-09 15:36:16 +0300

    added Is it a letter? probelm

---
+++ Codewars/7kyu/Is it a letter.py
@@ -0,0 +1,9 @@
+"""
+Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+
+"""
+
+def is_it_letter(s):
+    letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    return s in letters
