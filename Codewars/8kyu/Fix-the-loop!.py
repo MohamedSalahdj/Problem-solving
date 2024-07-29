caaf54a833ab00277d1d4b55df9a7c7d928f54e4
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-07-29 15:56:26 +0300

    added Define a card suit, Cat years, Dog years, Fix the loop! probelms

---
+++ Codewars/8kyu/Fix-the-loop!.py
@@ -0,0 +1,20 @@
+
+"""
+Description:
+Your collegue wrote an simple loop to list his favourite animals. But there seems to be a minor mistake in the grammar, which prevents the program to work. Fix it! :)
+
+If you pass the list of your favourite animals to the function, you should get the list of the animals with orderings and newlines added.
+
+For example, passing in:
+    animals = [ 'dog', 'cat', 'elephant' ]
+
+will result in:
+    list_animals(animals) == '1. dog\n2. cat\n3. elephant\n'
+"""
+
+
+def list_animals(animals):
+    lst = ''
+    for i in range(len(animals)):
+        lst += str(i + 1) + '. ' + animals[i] + '\n'
+    return lst
