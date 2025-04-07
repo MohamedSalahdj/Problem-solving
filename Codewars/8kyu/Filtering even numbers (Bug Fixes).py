fc1c035dfe8fb038c00a7b2d4f43775ae0f1db6a
MohamedSalahdj <mohamedsamydj@gmail.com>
2025-04-07 15:16:31 +0200

    Added second solution for  `Filtering even numbers (Bug Fixes)` problem

--- Codewars/8kyu/Filtering even numbers (Bug Fixes).py
+++ Codewars/8kyu/Filtering even numbers (Bug Fixes).py
@@ -4,6 +4,7 @@ The method is supposed to remove even numbers from the list and return a list th
 However, there is a bug in the method that needs to be resolved.
 """
 
+# First Solution
 def kata_13_december(lst): 
     i = 0
     while i < len(lst):
@@ -14,3 +15,8 @@ def kata_13_december(lst):
     return lst
 
 print(kata_13_december([1, 2, 2, 2, 4, 3, 4, 5, 6, 7]))
+
+
+# Second solution 
+def kata_13_december(lst): 
+    return [ item for item in lst if item%2 !=0 ] 
