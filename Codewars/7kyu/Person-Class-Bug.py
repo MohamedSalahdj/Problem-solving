0f91724862e935b9ea73d6c5b87b0b3303154e1a
Mohamed Salah <mohamedsamydj@gmail.com>
2025-10-03 23:16:00 +0300

    Fix Person class to correctly initialize age attribute

---
+++ Codewars/7kyu/Person-Class-Bug.py
@@ -0,0 +1,30 @@
+"""
+    The following code was thought to be working properly, however when the code tries to access the age of the person instance it fails.
+
+    person = Person('Yukihiro', 'Matsumoto', 47)
+    print(person.full_name)
+    print(person.age)
+    For this exercise you need to fix the code so that it works correctly.
+
+    Note: the order of the person's full name is first name and last name.
+"""
+
+# Old code 
+'''
+class Person():
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+
+'''
+
+class Person():
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
