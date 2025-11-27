8feac0125dc6a3706871fa3a276fa54c1d800a17
Mohamed Salah <mohamedsamydj@gmail.com>
2025-11-27 14:24:06 +0200

    Add Lamp class implementation with toggle and state methods

---
+++ Codewars/6kyu/The Lamp: Revisited.py
@@ -0,0 +1,21 @@
+"""
+Define a class called Lamp. It will have a string attribute for color and boolean attribute, on, 
+
+that will refer to whether the lamp is on or not. Define your class constructor with a parameter for color and assign on as false on initialize.
+
+Give the lamp an instance method called toggle_switch that will switch the value of the on attribute.
+
+Define another instance method called state that will return "The lamp is on." if it's on and "The lamp is off." otherwise.
+"""
+
+class Lamp:
+    def __init__(self, color: str, on=False):
+        self.color = color
+        self.on = on
+    
+    def toggle_switch(self):
+        self.on = not self.on
+    
+    def state(self) -> str:
+        return "The lamp is {}.".format("on" if self.on else "off")
+
