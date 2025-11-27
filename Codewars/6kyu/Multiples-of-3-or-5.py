1e3377c15ed44bed80eaef86bd54ae04673e8237
Mohamed Salah <mohamedsamydj@gmail.com>
2025-11-27 13:53:15 +0200

    Add solution function to calculate the sum of multiples of 3 or 5 below a given number

---
+++ Codewars/6kyu/Multiples-of-3-or-5.py
@@ -0,0 +1,24 @@
+"""
+Description:
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+    Additionally, if the number is negative, return 0.
+
+    Note: If a number is a multiple of both 3 and 5, only count it once.
+
+    Courtesy of projecteuler.net (Problem 1)
+"""
+
+def solution(number):
+    if number < 0:
+        return 0
+    
+    sum_numbers = 0
+    numbers = range(1, number)
+    
+    for number in numbers:
+        if number % 3 == 0 or number % 5 == 0:
+            sum_numbers += number
+    return sum_numbers
