ced5ca16a4c150d6177f8f0e1290156b9c5b42bf
Mohamed Salah <mohamedsamydj@gmail.com>
2025-12-02 21:04:29 +0200

    Add Song class implementation with title, artist, and listener tracking functionality

---
+++ Codewars/7kyu/What-a-"Classy"-Song.py
@@ -0,0 +1,46 @@
+"""
+Description:
+    Your job is to create a class called Song.
+
+    A new Song will take two parameters, title and artist.
+
+    mount_moose = Song('Mount Moose', 'The Snazzy Moose')
+
+    mount_moose.title => 'Mount Moose'
+    mount_moose.artist => 'The Snazzy Moose'
+    You will also have to create an instance method named howMany() (or how_many()).
+    The method takes an array of people who have listened to the song that day. The output should be how many new listeners the song gained on that day out of all listeners. Names should be treated in a case-insensitive manner, i.e. "John" is the same as "john".
+
+Example
+    mount_moose = Song('Mount Moose', 'The Snazzy Moose')
+    # day 1 (or test 1)
+    mount_moose.how_many(['John', 'Fred', 'BOb', 'carl', 'RyAn']) => 5
+    # These are all new since they are the first listeners.
+
+    # day 2
+    mount_moose.how_many(['JoHn', 'Luke', 'AmAndA']); => 2
+    # Luke and Amanda are new, john already listened to it the previous day
+    Also if the same person listened to it more than once a day it should only count them once.
+
+Example
+    mount_moose = Song('Mount Moose', 'The Snazzy Moose')
+
+    # day 1
+    mount_moose.how_many(['John', 'joHN', 'carl']) => 2
+"""
+
+
+class Song:
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+        self.listeners = set()
+    
+    def how_many(self, people):
+        new_count = 0
+        normalized = {p.lower() for p in people}
+        for person in normalized:
+            if person not in self.listeners:
+                new_count += 1
+                self.listeners.add(person)
+        return new_count
